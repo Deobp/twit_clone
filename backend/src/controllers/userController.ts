e75a42e19134b83428c7097c8b422e0cea35e92c
Oleksandr <d43z61p0@gmail.com>
2024-10-12 18:04:34 +0300

    refactor createUser controller

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,21 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
-const {User} = require('../models/UsersModel')
+const { User } = require('../models/UsersModel');
 
 export const createUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     console.log('request body from createUser:', req.body);
-    const {id, username, password} = req.body;
-    if(!id || !username || !password) {
-      res.status(400).json({message:'error with passing id / username/ pass'});
-      return null;
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).json({ message: 'Username and password are required' });
+      return;
     }
 
-    const newUser = await User.create({username: username, password});
+    const newUser = await User.create({ username, password });
     if (!newUser) {
-      res.status(400).json({message:'problem creating user'});
+      res.status(400).json({ message: 'Problem creating user' });
+      return;
     }
-    res.status(200).json({message: `user created: ${newUser}`});
+    res.status(201).json({ message: 'User created successfully', user: newUser });
   } catch (error) {
     next(error);
   }
-}
+};
